f6d57fdc1f0e37f9d5a166804ea2796441b12fe6
Lorenzo <lorenzozar@gmail.com>
2021-01-25 23:09:59 +0200

    basic types

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,11 @@
 function startGame() {
   let messagesElement = document.getElementById("messages");
+  let playerName: string = "Mon";
+  logPlayer(playerName);
   messagesElement!.innerText = "Welcome to multimath. Start..";
 }
 document.getElementById("startGame")!.addEventListener("click", startGame);
+
+function logPlayer(name: string | "Amar") {
+  console.log(`New game for ${name}!`);
+}
--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 "use strict";
 function startGame() {
     var messagesElement = document.getElementById("messages");
+    var playerName = "Mon";
+    logPlayer(playerName);
     messagesElement.innerText = "Welcome to multimath. Start..";
 }
 document.getElementById("startGame").addEventListener("click", startGame);
+function logPlayer(name) {
+    console.log("New game for " + name + "!");
+}
 //# sourceMappingURL=app.js.map
