700830610c638b53df911d0d2f4467e17ece10ed
Lorenzo <lorenzozar@gmail.com>
2021-01-25 22:06:14 +0200

    setup tsconfig

--- app/app.js
+++
@@ -1,5 +0,0 @@
-function startGame() {
-    var messagesElement = document.getElementById("messages");
-    messagesElement.innerText = "Welcome to multimath. Start..";
-}
-document.getElementById("startGame").addEventListener("click", startGame);
