53e838f5df8671a7001967b58c97bef75a82e2da
Lorenzo <lorenzozar@gmail.com>
2021-01-25 22:00:55 +0200

    event listener to start btn

---
+++ app/app.ts
@@ -0,0 +1,5 @@
+function startGame() {
+  let messagesElement = document.getElementById("messages");
+  messagesElement.innerText = "Welcome to multimath. Start..";
+}
+document.getElementById("startGame").addEventListener("click", startGame);
