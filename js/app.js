8a577316488f409bc2a3d8eb97c9e8b8acf24a27
Lorenzo <lorenzozar@gmail.com>
2021-01-25 22:13:12 +0200

    tsconfig project level

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function startGame() {
+    var messagesElement = document.getElementById("messages");
+    messagesElement.innerText = "Welcome to multimath. Start..";
+}
+document.getElementById("startGame").addEventListener("click", startGame);
+//# sourceMappingURL=app.js.map
