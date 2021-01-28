3e3d2109899f1757b6b7a19ed06f09564ea5371c
Lorenzo <lorenzozar@gmail.com>
2021-01-28 05:52:05 +0200

    get data from input

--- app/app.ts
+++ app/app.ts
@@ -1,11 +1,32 @@
 function startGame() {
   let messagesElement = document.getElementById("messages");
-  let playerName: string = "Mon";
+  let playerName: string | undefined = getInputValue("playername");
+
   logPlayer(playerName);
-  messagesElement!.innerText = "Welcome to multimath. Start..";
+  postScore(100, playerName);
+  // messagesElement!.innerText = "Welcome to multimath. Start..";
 }
 document.getElementById("startGame")!.addEventListener("click", startGame);
 
-function logPlayer(name: string | "Amar") {
+function logPlayer(name: string = "Amar"): void {
   console.log(`New game for ${name}!`);
+}
+
+function getInputValue(elementID: string): string | undefined {
+  const inputElement: HTMLInputElement = <HTMLInputElement>(
+    document.getElementById(elementID)
+  );
+
+  if (inputElement.value === "") {
+    return undefined;
+  } else {
+    return inputElement.value;
+  }
+}
+
+function postScore(score: number, playerName: string = "Mori"): void {
+  const scoreElement: HTMLElement | null = document.getElementById(
+    "postedScores"
+  );
+  scoreElement!.innerText = `${score} - ${playerName}`;
 }
